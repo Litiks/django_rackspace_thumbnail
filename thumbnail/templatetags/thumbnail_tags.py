21916b2989fa6ddba79f2dc396faf23acf743093
Aaron Mader <a.mader@gmail.com>
2014-06-02 16:59:42 -0400

    adding square thumbnail option (which crops the image)

--- thumbnail/templatetags/thumbnail_tags.py
+++ thumbnail/templatetags/thumbnail_tags.py
@@ -1,5 +1,8 @@
 import os
-import Image
+try:
+    from PIL import Image
+except:
+    import Image
 from cStringIO import StringIO
 from uuid import uuid4
 
@@ -10,7 +13,7 @@ from thumbnail.models import Thumbnail
 
 register = Library()
 
-def thumbnail(file, size='104x104'):
+def thumbnail(file, size='104x104', square=False):
     """
     Returns a thumbnail image of the specified file. (Rendering one if necessary)
     """
@@ -21,7 +24,10 @@ def thumbnail(file, size='104x104'):
     x, y = [int(x) for x in size.split('x')]
     # defining the filename and the miniature filename
     basename, format = os.path.splitext(file.name)
-    miniature = basename + '_' + size + format
+    if square:
+        miniature = basename + '_' + size + '_sq' + format
+    else:
+        miniature = basename + '_' + size + format
     
     #do we have a thumbnail by this name?
     thumbnails = Thumbnail.objects.filter(name=miniature)
@@ -39,6 +45,26 @@ def thumbnail(file, size='104x104'):
             return None
 
         #Do the work
+        if square:
+            width, height = image.size
+            if width > height:
+                delta = width - height
+                left = int(delta/2)
+                upper = 0
+                right = height + left
+                lower = height
+            else:
+                delta = height - width
+                left = 0
+                upper = int(delta/2)
+                right = width
+                lower = width + upper
+
+            # carryover the image format manually (not sure why this doesn't work automatically)
+            image_format = image.format
+            image = image.crop((left, upper, right, lower))
+            image.format = image_format
+
         image.thumbnail([x, y], Image.ANTIALIAS)
         
         # now, save it
@@ -62,3 +88,7 @@ def thumbnail(file, size='104x104'):
 
 register.filter(thumbnail)
 
+def square_thumbnail(file, size='104'):
+    return thumbnail(file, "%sx%s" % (size, size), square=True)
+
+register.filter(square_thumbnail)
